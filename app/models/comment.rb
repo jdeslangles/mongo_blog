153cfb2df45055d97e52d51e3356237f6a214b0c
jdeslangles <julien.deslangles@gmail.com>
2013-12-02 16:55:04 +0000

    added Comments model

---
+++ app/models/comment.rb
@@ -0,0 +1,6 @@
+class Comment
+  include MongoMapper::EmbeddedDocument
+  key :content, String
+  belongs_to :user
+  timestamps!
+end
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,10 @@ class Post
   include MongoMapper::Document
   key :title, String
   key :content, String
+  key :view_count, Integer, default: 0
+  
   timestamps!
   belongs_to :user
+  has_many :comments
+
 end
