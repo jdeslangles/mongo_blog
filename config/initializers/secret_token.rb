ba3cd439d108ba63e993adea0c9aa24aba58ec5c
jdeslangles <julien.deslangles@gmail.com>
2013-12-02 16:08:18 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MongoBlog::Application.config.secret_token = '75c62157ec5d29bb63a28e896157aa088be2568feea90fdd8e3045f524a9484daa0561070872acdbec8ad5c4a399ec33dae0cd555f8cacce58ed6c1be976b791'
