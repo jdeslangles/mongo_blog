2bbff459a4b1eb6b028aaa4355fe5137596af9d0
jdeslangles <julien.deslangles@gmail.com>
2013-12-02 16:58:50 +0000

    scope added in post.rb

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,9 +3,13 @@ class Post
   key :title, String
   key :content, String
   key :view_count, Integer, default: 0
-  
+
   timestamps!
   belongs_to :user
   has_many :comments
+
+  def self.flagged
+    where(flagged:true)
+  end
 
 end
